77ae2299d7d2c8e8975e8268d17a4d289f1c6de9
Emanuelecozzolino1926 <emanuele.cozzolino18@gmail.com>
2025-11-25 15:14:44 +0100

    [UPD] Boolist and single book terminated

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,62 +1,59 @@
-import { Component } from 'react'
-import SingleBook from './SingleBook'
-import { Col, Form, Row } from 'react-bootstrap'
-import CommentArea from './CommentArea'
+import { useState } from "react";
+import SingleBook from "./SingleBook";
+import { Col, Form, Row } from "react-bootstrap";
+import CommentArea from "./CommentArea";
 
-class BookList extends Component {
-  state = {
-    searchQuery: '',
-    selectedBook: null,
-  }
+const BookList = (props) => {
+  // state = {
+  //   searchQuery: "",
+  //   selectedBook: null,
+  // };
 
-  changeSelectedBook = (asin) => {
-    this.setState({
-      selectedBook: asin,
-    })
-  }
+  const [searchQuery, setSearchQuery] = useState("");
+  const [selectedBook, setSelectedBook] = useState(null);
 
-  render() {
-    return (
-      <>
-        <Row>
-          <Col md={8}>
-            <Row className="justify-content-center mt-5">
-              <Col xs={12} md={4} className="text-center">
-                <Form.Group>
-                  <Form.Control
-                    type="search"
-                    placeholder="Cerca un libro"
-                    value={this.state.searchQuery}
-                    onChange={(e) =>
-                      this.setState({ searchQuery: e.target.value })
-                    }
-                  />
-                </Form.Group>
+  const changeSelectedBook = (asin) => {
+    setSelectedBook(asin);
+  };
+
+  const filteredBooks = props.books.filter((b) =>
+    b.title.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
+  return (
+    <>
+      <Row>
+        <Col md={8}>
+          <Row className="justify-content-center mt-5">
+            <Col xs={12} md={4} className="text-center">
+              <Form.Group>
+                <Form.Control
+                  type="search"
+                  placeholder="Cerca un libro"
+                  value={searchQuery}
+                  onChange={(e) => setSearchQuery(e.target.value)}
+                />
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row className="g-2 mt-3">
+            {filteredBooks.map((b) => (
+              <Col xs={12} md={4} key={b.asin}>
+                <SingleBook
+                  book={b}
+                  selectedBook={selectedBook}
+                  changeSelectedBook={changeSelectedBook}
+                />
               </Col>
-            </Row>
-            <Row className="g-2 mt-3">
-              {this.props.books
-                .filter((b) =>
-                  b.title.toLowerCase().includes(this.state.searchQuery)
-                )
-                .map((b) => (
-                  <Col xs={12} md={4} key={b.asin}>
-                    <SingleBook
-                      book={b}
-                      selectedBook={this.state.selectedBook}
-                      changeSelectedBook={this.changeSelectedBook}
-                    />
-                  </Col>
-                ))}
-            </Row>
-          </Col>
-          <Col md={4}>
-            <CommentArea asin={this.state.selectedBook} />
-          </Col>
-        </Row>
-      </>
-    )
-  }
-}
+            ))}
+          </Row>
+        </Col>
+        <Col md={4}>
+          <CommentArea asin={selectedBook} />
+        </Col>
+      </Row>
+    </>
+  );
+};
 
-export default BookList
+export default BookList;
